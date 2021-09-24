d5e2fb395ea973e16e070c0e581534ffb6c2ff07
pei.Tang <pea.tang@qq.com>
2021-09-24 18:34:53 +0800

    fix code bug

--- src/listing_2.1.cpp
+++ src/listing_2.1.cpp
@@ -1,31 +1,31 @@
 #include <thread>
 #include <iostream>
 
-unsing namespace std;
+// using namespace std;
 
 void do_something(int& i) {
-    ++i;
+  std::cout << "HELLO\n";
+  ++i;
 }
 
 struct func {
-    int& i;
-
-    func(int& i_) : i(i_) {}
-
-    void operator()() {
-        for (unsigned j = 0; j < 1000000; ++j) {
-            do_something(i);
-        }
+  int& i;
+  func(int& i_) : i(i_) {}
+  void operator()() {
+    for (unsigned j = 0; j < 1000000; ++j) {
+      do_something(i);
     }
+  }
 };
 
 void oops() {
-    int some_local_state = 0;
-    func my_func(some_local_state);
-    std::thread my_thread(my_func);
-    my_thread.detach();
+  int some_local_state = 0;
+  func my_func(some_local_state);
+  std::thread my_thread((my_func));
+  my_thread.detach();
 }
 
 int main() {
-    oops();
+    std::cout<<"hello\n";
+  oops();
 }
--- src/listing_2.5.cpp
+++ src/listing_2.5.cpp
@@ -1,27 +1,26 @@
 #include <thread>
-
-void some_function()
-{}
-
-void some_other_function(int)
-{}
-
-std::thread f()
-{
-    void some_function();
-    return std::thread(some_function);
-}
-std::thread g()
-{
-    void some_other_function(int);
-    std::thread t(some_other_function,42);
-    return t;
+#include <iostream>
+void some_function() {
+  std::cout << "hello" << std::endl;
 }
 
-int main()
-{
-    std::thread t1=f();
-    t1.join();
-    std::thread t2=g();
-    t2.join();
+void some_other_function(int) {
+  std::cout << "\thello" << std::endl;
+}
+
+std::thread f() {
+  void some_function();
+  return std::thread(some_function);
+}
+std::thread g() {
+  void some_other_function(int);
+  std::thread t(some_other_function, 42);
+  return t;
+}
+
+int main() {
+  std::thread t1 = f();
+  t1.join();
+  std::thread t2 = g();
+  t2.join();
 }
